9c6231e83f602f2a93535fa408c52657446877e1
afg2002 <afghanekapangestu@gmail.com>
2022-08-11 21:29:51 +0700

    Fix the front page to display current data

--- controller/main_controller.go
+++ controller/main_controller.go
@@ -4,6 +4,7 @@ import (
 	"html/template"
 	"net/http"
 	"os"
+	"perpustakaan/db"
 	"perpustakaan/entity"
 	"perpustakaan/helper"
 
@@ -20,12 +21,30 @@ func HandlerIndex(w http.ResponseWriter, r *http.Request) {
 
 	session, _ := store.Get(r, "session_login")
 	t := template.Must(template.ParseFiles("./views/base.gohtml", "./views/index.gohtml"))
+
+	// Get the book data
+	con := db.ConnectionDB()
+	sql := "SELECT * FROM buku"
+	result, err := con.Query(sql)
+	helper.PanicIfError(err)
+
+	book := entity.Book{}
+	var resp []entity.Book
+	for result.Next() {
+		err = result.Scan(&book.ID, &book.Cover, &book.Judul, &book.Pengarang, &book.Kategori, &book.Penerbit, &book.Tahun, &book.Stok)
+		helper.PanicIfError(err)
+
+		resp = append(resp, book)
+	}
+	data.BookData = resp
+
 	data.SessionData.Title = "Go Perpus"
 	data.SessionData.Auth = session.Values["auth"]
 	data.SessionData.Email = session.Values["email"]
 	data.SessionData.Name = session.Values["name"]
 	data.SessionData.Role = session.Values["role"]
 	data.SessionData.Message = session.Values["message"]
-	err := t.ExecuteTemplate(w, base, &data)
+	err = t.ExecuteTemplate(w, base, &data)
 	helper.PanicIfError(err)
+
 }
